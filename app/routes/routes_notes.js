ed2b7e8f9737609733e9a4ae69814cf3049a9821
Joris <joris.onghena@uantwerpen.be>
2019-06-13 17:18:01 +0200

    Added note routes

--- app/routes/routes_notes.js
+++ app/routes/routes_notes.js
@@ -3,4 +3,22 @@ module.exports = (app, db) => {
     app.get('/notes', (req, res) => {  
         res.send('Hello there !')
     });
+
+    app.get('/notes/:id', (req, res) => {  
+        const id = req.params.id;
+        res.send(`You requested note: ${id}`);
+    });
+
+    app.post('/notes', (req, res) => {  
+        res.send('You create a note !');
+    });
+
+    app.put('/notes', (req, res) => {  
+        res.send('You updated a note !');
+    });
+
+    app.delete('/notes/:id', (req, res) => {  
+        const id = req.params.id;
+        res.send(`You deleted note ${id} !`);
+    });
 }
