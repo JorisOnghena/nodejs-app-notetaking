a3a18145c1e2348f3e57c9dfa83d3adb51f4b879
Joris <joris.onghena@uantwerpen.be>
2019-06-13 17:03:55 +0200

    Added body-parser package

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 54321;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Call index to load all routes with our app vaiable.
 const routes = require('./app/routes');
